759e03e05f3e21b4847dad05e206b70d91d2a477
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-02-22 19:36:16 -0500

    linked Hero and Villain project to nysql database, Create super and super type app

--- heros_villains_project/settings.py
+++ heros_villains_project/settings.py
@@ -111,3 +111,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from heros_villains_project .local_setting import *
+except ImportError:
+    pass
