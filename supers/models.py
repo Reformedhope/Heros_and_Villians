6244bcf10730a156d189cef45ba7b7adbc61ca8a
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-02-22 19:59:51 -0500

    updated Admin file +created super class + created serializers +updated supers app

--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Supers(models.Model):
+    name = models.CharField(max_length=255)
+    alter_ego = models.CharField(max_length=255)
+    primary_ability = models.CharField(max_length=255)
+    secondary_ability = models.CharField(max_length=255)
+    catch_phrase = models.CharField(max_length=255)
+    
+
+
 # Create your models here.
